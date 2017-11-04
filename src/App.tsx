0b575eca0ebf19516a5406892e4881875a05b294
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-04 18:08:04 +0100

    Set up css and fonts structure
    #2 #28

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import * as React from "react";
+import {TitleHeader} from "./TitleHeader";
+import {css, StyleSheet} from "aphrodite";
 
 export class App extends React.Component {
   render () {
-    return "Hello World";
+    return (
+      <div className={css(styles.base)}>
+        <TitleHeader/>
+        <div>
+          Hello World
+        </div>
+      </div>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  base: {
+    fontFamily: "Ubuntu"
+  }
+});
