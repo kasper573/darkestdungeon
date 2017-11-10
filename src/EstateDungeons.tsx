ba8c51aabc02ea8acb2d01629bf942946bdf31b9
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-10 14:45:51 +0100

    Add embark warnings to estate sections

--- src/EstateDungeons.tsx
+++ src/EstateDungeons.tsx
@@ -2,13 +2,26 @@ import * as React from "react";
 import {AppState} from "./AppState";
 import {EstateTemplate} from "./EstateTemplate";
 import {Path} from "./RouterState";
+import {Prompt} from "./Popups";
 
 export class EstateDungeons extends React.Component<{state: AppState, path: Path}> {
+  checkPartyBeforeContinue () {
+    return this.props.state.popups.prompt(
+      <Prompt
+        query={"Grave danger awaits the underprepared. " +
+        "Do you wish to continue without a full contingent?"}
+        yesLabel="Still Embark"
+        noLabel="Cancel Embark"
+      />
+    );
+  }
+
   render () {
     return (
       <EstateTemplate
         state={this.props.state}
         path={this.props.path}
+        continueCheck={() => this.checkPartyBeforeContinue()}
         continueLabel="Provision"
         continuePath="estateProvision">
         Estate dungeon map
--- src/EstateProvision.tsx
+++ src/EstateProvision.tsx
@@ -2,13 +2,24 @@ import * as React from "react";
 import {AppState} from "./AppState";
 import {EstateTemplate} from "./EstateTemplate";
 import {Path} from "./RouterState";
+import {Prompt} from "./Popups";
 
 export class EstateProvision extends React.Component<{state: AppState, path: Path}> {
+  checkItemsBeforeContinue () {
+    return this.props.state.popups.prompt(
+      <Prompt
+        query={"You haven't purchased much food for your expedition. " +
+        "It's recommended to take at least 8 food for this quest. Still Embark?"}
+      />
+    );
+  }
+
   render () {
     return (
       <EstateTemplate
         state={this.props.state}
         path={this.props.path}
+        continueCheck={() => this.checkItemsBeforeContinue()}
         continueLabel="Embark"
         continuePath={new Path("loading", {target: "dungeon"})}>
         Estate provision
