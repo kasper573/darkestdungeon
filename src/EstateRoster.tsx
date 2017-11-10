94a1fae6272d0cff2378ea83b11a5ee4c3b884b2
Kasper <805006+kasper573@users.noreply.github.com>
2017-11-10 20:33:48 +0100

    Create estate roster component

---
+++ src/EstateRoster.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import {AppState} from "./AppState";
+import {css, StyleSheet} from "aphrodite";
+import {EstateRosterEntry} from "./EstateRosterEntry";
+import {Character} from "./ProfileData";
+import {observer} from "mobx-react";
+import {SortOptions} from "./SortOptions";
+import {CharacterInfo} from "./CharacterInfo";
+import {ModalState, PopupAlign, PopupHandle} from "./PopupState";
+
+@observer
+export class EstateRoster extends React.Component<{state: AppState}> {
+  characterInfoPopup?: PopupHandle;
+
+  componentWillUnmount () {
+    if (this.characterInfoPopup) {
+      this.characterInfoPopup.close();
+    }
+  }
+
+  showCharacterInfo (character: Character) {
+    if (this.characterInfoPopup) {
+      this.characterInfoPopup.close();
+    }
+
+    this.characterInfoPopup = this.props.state.popups.show({
+      content: <CharacterInfo character={character}/>,
+      align: PopupAlign.TopLeft,
+      position: {x: 50, y: 50},
+      modalState: ModalState.Opaque
+    });
+  }
+
+  render () {
+    const profile = this.props.state.profiles.activeProfile;
+    const characters = profile.characters;
+    const sortedCharacters = characters.slice().sort(Character.comparers.rosterIndex);
+    return (
+      <div className={css(styles.roster)}>
+        <div className={css(styles.header)}>
+          <span>
+            {profile.characters.length} / {profile.rosterSize}
+          </span>
+          <SortOptions
+            comparers={Character.visibleComparers}
+            onChange={(fn) => profile.sortCharacters(fn)}
+          />
+        </div>
+        <ul>
+          {sortedCharacters.map((character) => (
+            <EstateRosterEntry
+              key={character.id}
+              character={character}
+              onSelect={() => this.showCharacterInfo(character)}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  roster: {
+    position: "absolute",
+    top: 0, right: 0
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center"
+  }
+});
