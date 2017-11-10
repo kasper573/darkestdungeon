6ad9416540ae94a943dfbb3991225611129ab3fc
Kasper <805006+kasper573@users.noreply.github.com>
2017-11-10 14:10:06 +0100

    Add common popup templates

---
+++ src/LineButton.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import {css, StyleSheet} from "aphrodite";
+
+export class LineButton extends React.Component<{
+  label: string,
+  className?: string,
+  onClick?: () => void
+}> {
+  render () {
+    return (
+      <span className={css(styles.lineButton) + " " + this.props.className} onClick={this.props.onClick}>
+        {this.props.label}
+      </span>
+    );
+  }
+}
+
+function dynamicStyle (borderColor: string, shadowColor: string, glowSize: number) {
+  return {
+    borderTop: `2px solid ${borderColor}`,
+    borderBottom: `2px solid ${borderColor}`,
+    textShadow: `0px 0px ${glowSize}px ${shadowColor}`,
+  };
+}
+
+const styles = StyleSheet.create({
+  lineButton: {
+    ...dynamicStyle("transparent", "transparent", 0),
+    paddingTop: 4,
+    paddingBottom: 4,
+    fontWeight: "bold",
+    textAlign: "center",
+    overflow: "hidden",
+
+    transition: [
+      "border .1s ease-out",
+      "text-shadow .1s ease-out"
+    ].join(","),
+
+    ":hover": {
+      ...dynamicStyle("#352813", "#ffffff", 35),
+    }
+  }
+});
