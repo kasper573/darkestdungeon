afdc61362f86115177646c53e3877b3165032caa
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-10 20:33:05 +0100

    Create character info component

---
+++ src/CharacterInfo.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import {Character} from "./ProfileData";
+import {observer} from "mobx-react";
+import {Popup, PopupProps} from "./Popups";
+
+@observer
+export class CharacterInfo extends React.Component<
+  PopupProps & {
+  character: Character
+}> {
+  render () {
+    const {character, ...rest} = this.props;
+    return (
+      <Popup {...rest}>
+        <div>{character.name}</div>
+        <br/>
+        <h1>Trinkets:</h1>
+        <ul>
+          {character.trinkets.map((trinket) => (
+            <li key={trinket.id}>{trinket.name}</li>
+          ))}
+        </ul>
+      </Popup>
+    );
+  }
+}
