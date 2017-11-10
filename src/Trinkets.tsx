e90f9f874ba3e05aae762d47ea6bc60d06fc70cf
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-10 20:33:21 +0100

    Create trinkets component

---
+++ src/Trinkets.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import {Popup, PopupProps, Prompt} from "./Popups";
+import {Trinket} from "./ProfileData";
+import {observer} from "mobx-react";
+import {BannerHeader} from "./BannerHeader";
+import {CompareFunction, SortOptions} from "./SortOptions";
+import {observable} from "mobx";
+import {css, StyleSheet} from "aphrodite";
+import {AppState} from "./AppState";
+
+@observer
+export class Trinkets extends React.Component<
+  PopupProps & {
+  state: AppState,
+}> {
+  @observable compareFn: CompareFunction<Trinket>;
+
+  promptUnequipAll () {
+    this.props.state.popups.prompt(
+      <Prompt query="Unequip all trinkets on all heroes?"/>
+    ).then((unequip) => {
+      if (unequip) {
+        this.props.state.profiles.activeProfile.unequipAllTrinkets();
+      }
+    });
+  }
+
+  render () {
+    const profile = this.props.state.profiles.activeProfile;
+    const sortedTrinkets = profile.unassignedTrinkets.sort(this.compareFn);
+    return (
+      <Popup {...this.props}>
+        <BannerHeader>
+          Trinket Inventory
+        </BannerHeader>
+        <div className={css(styles.buttonBar)}>
+          <span onClick={() => this.promptUnequipAll()}>
+            [UNEQUIP]
+          </span>
+          <SortOptions
+            comparers={Trinket.comparers}
+            onChange={(compareFn) => this.compareFn = compareFn}
+          />
+        </div>
+        <ul>
+          {sortedTrinkets.map((trinket) => (
+            <li key={trinket.id}>
+              [{trinket.name}]
+            </li>
+          ))}
+        </ul>
+      </Popup>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonBar: {
+    flexDirection: "row"
+  }
+});
