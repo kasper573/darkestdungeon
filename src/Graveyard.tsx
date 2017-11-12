6c37dc5b487d61445a6d9ce230fb2d0f56d7edd2
Kasper <805006+kasper573@users.noreply.github.com>
2017-11-12 20:15:50 +0100

    Add estate buildings
    Fixes #12 Fixes #13 Fixes #15 Fixes #14 Fixes #16 Fixes #17

---
+++ src/Graveyard.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {BuildingOverview} from "./BuildingOverview";
+
+export class Graveyard extends React.Component<{
+  header?: string
+}> {
+  static id = "graveyard";
+
+  render () {
+    return (
+      <BuildingOverview
+        header="Graveyard"
+        backgroundUrl={require("../assets/images/graveyard-bg.jpg")}/>
+    );
+  }
+}
---
+++ src/Tavern.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import {BuildingOverview} from "./BuildingOverview";
+
+export class Tavern extends React.Component<{
+  header?: string
+}> {
+  static id = "tavern";
+
+  render () {
+    return (
+      <BuildingOverview
+        header="Tavern"
+        backgroundUrl={require("../assets/images/tavern-bg.jpg")}
+      />
+    );
+  }
+}
