eedcb247a5745cc625ab95e416eff0ca3702e120
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-13 19:01:36 +0100

    Mock estate dungeon map

---
+++ src/EstateDungeonBreakdown.tsx
@@ -0,0 +1,37 @@
+
+import * as React from "react";
+import {Quest, QuestId} from "./ProfileState";
+import {css} from "aphrodite";
+import {commonStyles} from "./config/styles";
+import {QuestSelector} from "./QuestSelector";
+
+export class EstateDungeonBreakdown extends React.Component<{
+  name: string,
+  level: number,
+  progress: number,
+  quests: Quest[],
+  selectedQuestId: QuestId,
+  onQuestSelected: (q: Quest) => void
+}> {
+  static defaultProps: any = {
+    quests: []
+  };
+
+  render () {
+    return (
+      <div style={{textAlign: "center"}}>
+        <h1 className={css(commonStyles.dungeonName)}>{this.props.name}</h1>
+        <ul style={{flexDirection: "row"}}>
+          {this.props.quests.map((quest) =>
+            <QuestSelector
+              key={quest.id}
+              quest={quest}
+              isSelected={this.props.selectedQuestId === quest.id}
+              onSelected={() => this.props.onQuestSelected(quest)}
+            />
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
--- src/EstateDungeons.tsx
+++ src/EstateDungeons.tsx
@@ -3,10 +3,31 @@ import {AppState} from "./AppState";
 import {EstateTemplate} from "./EstateTemplate";
 import {Path} from "./RouterState";
 import {Prompt} from "./Popups";
+import {Quest} from "./ProfileState";
+import {observer} from "mobx-react";
+import {observable} from "mobx";
+import {EstateDungeonBreakdown} from "./EstateDungeonBreakdown";
+import {QuestBreakdown} from "./QuestBreakdown";
 
+@observer
 export class EstateDungeons extends React.Component<{state: AppState, path: Path}> {
+  @observable selectedQuest: Quest = Array.from(
+    this.props.state.profiles.activeProfile.quests
+  )[0];
+
   componentWillMount () {
     this.props.state.ambience.activate("estateDungeons");
+  }
+
+  groupQuestsByDungeon (quests: Quest[]) {
+    return quests.reduce(
+      (groups: any, quest) => {
+        const list = groups[quest.dungeonId] || (groups[quest.dungeonId] = []);
+        list.push(quest);
+        return groups;
+      },
+      {} as {[key: string]: Quest[]}
+    );
   }
 
   checkPartyBeforeContinue () {
@@ -21,6 +42,9 @@ export class EstateDungeons extends React.Component<{state: AppState, path: Path
   }
 
   render () {
+    const profile = this.props.state.profiles.activeProfile;
+    const questLookup = this.groupQuestsByDungeon(profile.quests);
+
     return (
       <EstateTemplate
         state={this.props.state}
@@ -29,7 +53,23 @@ export class EstateDungeons extends React.Component<{state: AppState, path: Path
         continueCheck={() => this.checkPartyBeforeContinue()}
         continueLabel="Provision"
         continuePath="estateProvision">
-        Estate dungeon map
+        {profile.dungeons.map((d) =>
+          <EstateDungeonBreakdown
+            key={d.id}
+            name={d.dungeonInfo.name}
+            level={d.level.number}
+            progress={d.levelProgress}
+            quests={questLookup[d.id]}
+            selectedQuestId={this.selectedQuest && this.selectedQuest.id}
+            onQuestSelected={(quest) => this.selectedQuest = quest}
+          />
+        )}
+        {this.selectedQuest && (
+          <QuestBreakdown
+            quest={this.selectedQuest}
+            popups={this.props.state.popups}
+          />
+        )}
       </EstateTemplate>
     );
   }
