34e96281dfcc7c0ea5910e8eed929c2db2170ff2
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-13 18:42:09 +0100

    Add basic style to CommonHeader

--- src/CommonHeader.tsx
+++ src/CommonHeader.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
+import {css} from "aphrodite";
+import {commonStyles} from "./config/styles";
 
 export class CommonHeader extends React.Component<{label: string}> {
   render () {
     return (
-      <div style={{whiteSpace: "nowrap"}}>{this.props.label}</div>
+      <div
+        className={css(commonStyles.commonName)}
+        style={{whiteSpace: "nowrap"}}>
+        {this.props.label}
+      </div>
     );
   }
 }
