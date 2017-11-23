1bcd83ecb21e4d56aee9fffe47afbe4a5a70613b
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-23 19:09:41 +0100

    Split up BuildingUpgradeShop components into separate files

---
+++ src/screens/estate/buildings/BuildingUpgradeIcon.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import {observer} from "mobx-react";
+import {AppStateComponent} from "../../../AppStateComponent";
+import {BuildingUpgradeInfo} from "../../../state/types/BuildingUpgradeInfo";
+import {BuildingInfo} from "../../../state/types/BuildingInfo";
+import {Prompt} from "../../../ui/Popups";
+import {Heirlooms} from "../../../ui/Heirlooms";
+import {TooltipArea} from "../../../lib/TooltipArea";
+import {UpgradeTooltip} from "./UpgradeTooltip";
+import {StyleSheet} from "aphrodite";
+
+@observer
+export class BuildingUpgradeIcon extends AppStateComponent<{
+  item: BuildingUpgradeInfo,
+  category: BuildingInfo,
+  prerequisite?: BuildingUpgradeInfo,
+  level: number
+}> {
+  async promptUnlock () {
+    const proceed = await this.appState.popups.prompt(
+      <Prompt query={(
+        <div>
+          Do you wish to purchase this upgrade for <Heirlooms counts={this.props.item.cost}/>?
+        </div>
+      )}/>
+    );
+
+    if (proceed) {
+      this.activeProfile.purchaseUpgrade(this.props.item);
+    }
+  }
+
+  render () {
+    const {item} = this.props;
+
+    const isUpgraded = this.activeProfile.ownsUpgrade(item);
+    const canAfford = this.activeProfile.hasEnoughHeirlooms(item.cost);
+    const isAvailable = !this.props.prerequisite || this.activeProfile.ownsUpgrade(this.props.prerequisite);
+    const canBuy = canAfford && isAvailable && !isUpgraded;
+
+    const dynStyle = isUpgraded ? styles.stepOwned : (isAvailable ? styles.stepAvailable : styles.stepLocked);
+    const onClick = canBuy ? () => this.promptUnlock() : undefined;
+
+    return (
+      <div>
+        <TooltipArea
+          onClick={onClick}
+          classStyle={[styles.step, dynStyle]}
+          tip={(
+            <UpgradeTooltip
+              cost={<Heirlooms counts={item.cost} compare={this.activeProfile.heirloomCounts}/>}
+              isAvailable={isAvailable}
+              prerequisiteName={this.props.category.name}
+              prerequisiteLevel={this.props.level - 1}
+            >
+              <pre>{item.description}</pre>
+            </UpgradeTooltip>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export const stepSize = 15;
+const styles = StyleSheet.create({
+  step: {
+    width: stepSize,
+    height: stepSize,
+    border: "1px solid gray",
+
+    ":hover": {
+      boxShadow: "0px 0px 4px white"
+    }
+  },
+
+  stepOwned: {
+    backgroundColor: "gold",
+    border: "1px solid gold"
+  },
+
+  stepAvailable: {
+    backgroundColor: "gray"
+  },
+
+  stepLocked: {
+    backgroundColor: "red"
+  }
+});
--- src/screens/estate/buildings/BuildingUpgradeShop.tsx
+++ src/screens/estate/buildings/BuildingUpgradeShop.tsx
@@ -1,17 +1,10 @@
 import * as React from "react";
-import {Column, commonStyles, Row} from "../../../config/styles";
+import {Column, Row} from "../../../config/styles";
 import {BannerHeader} from "../../../ui/BannerHeader";
-import {css, StyleSheet} from "aphrodite";
-import {Avatar} from "../../../ui/Avatar";
-import {TooltipArea} from "../../../lib/TooltipArea";
-import {BuildingUpgradeInfo} from "../../../state/types/BuildingUpgradeInfo";
 import {AppStateComponent} from "../../../AppStateComponent";
-import {Prompt} from "../../../ui/Popups";
-import {observer} from "mobx-react";
 import {findSubset} from "../../../lib/Helpers";
 import {BuildingInfo} from "../../../state/types/BuildingInfo";
-import {Heirlooms} from "../../../ui/Heirlooms";
-import {UpgradeTooltip} from "./UpgradeTooltip";
+import {BuildingUpgradeCategory} from "./BuildingUpgradeCategory";
 
 export class BuildingUpgradeShop extends AppStateComponent<{
   upgrades: BuildingInfo
@@ -20,7 +13,7 @@ export class BuildingUpgradeShop extends AppStateComponent<{
     const categories: any[] = [];
     this.props.upgrades.children.forEach((category) => {
       categories.push(
-        <UpgradeCategory key={category.key} category={category}/>
+        <BuildingUpgradeCategory key={category.key} category={category}/>
       );
     });
 
@@ -44,147 +37,3 @@ export class BuildingUpgradeShop extends AppStateComponent<{
     );
   }
 }
-
-class UpgradeCategory extends React.Component<{
-  category: BuildingInfo
-}> {
-  render () {
-    const steps: any[] = [];
-    this.props.category.items.forEach((itemInfo, index) => {
-      const prerequisite = this.props.category.items[index - 1];
-      steps.push(<Line key={"line" + index}/>);
-      steps.push(
-        <UpgradeItem
-          key={itemInfo.id}
-          item={itemInfo}
-          category={this.props.category}
-          level={index + 1}
-          prerequisite={prerequisite}/>
-      );
-    });
-
-    return (
-      <div className={css(styles.upgrade)}>
-        <h1 className={css(commonStyles.upgradeName)}>{this.props.category.name}</h1>
-        <Row classStyle={styles.upgradeSequence}>
-          <TooltipArea
-            tip={this.props.category.description}
-            classStyle={styles.upgradeAvatar}>
-            <Avatar src={this.props.category.avatarUrl}/>
-          </TooltipArea>
-          {steps}
-        </Row>
-      </div>
-    );
-  }
-}
-
-@observer
-class UpgradeItem extends AppStateComponent<{
-  item: BuildingUpgradeInfo,
-  category: BuildingInfo,
-  prerequisite?: BuildingUpgradeInfo,
-  level: number
-}> {
-  async promptUnlock () {
-    const proceed = await this.appState.popups.prompt(
-      <Prompt query={(
-        <div>
-          Do you wish to purchase this upgrade for <Heirlooms counts={this.props.item.cost}/>?
-        </div>
-      )}/>
-    );
-
-    if (proceed) {
-      this.activeProfile.purchaseUpgrade(this.props.item);
-    }
-  }
-
-  render () {
-    const {item} = this.props;
-
-    const isUpgraded = this.activeProfile.ownsUpgrade(item);
-    const canAfford = this.activeProfile.hasEnoughHeirlooms(item.cost);
-    const isAvailable = !this.props.prerequisite || this.activeProfile.ownsUpgrade(this.props.prerequisite);
-    const canBuy = canAfford && isAvailable && !isUpgraded;
-
-    const dynStyle = isUpgraded ? styles.stepOwned : (isAvailable ? styles.stepAvailable : styles.stepLocked);
-    const onClick = canBuy ? () => this.promptUnlock() : undefined;
-
-    return (
-      <div>
-        <TooltipArea
-          onClick={onClick}
-          classStyle={[styles.step, dynStyle]}
-          tip={(
-            <UpgradeTooltip
-              cost={<Heirlooms counts={item.cost} compare={this.activeProfile.heirloomCounts}/>}
-              isAvailable={isAvailable}
-              prerequisiteName={this.props.category.name}
-              prerequisiteLevel={this.props.level - 1}
-            >
-              <pre>{item.description}</pre>
-            </UpgradeTooltip>
-          )}
-        />
-      </div>
-    );
-  }
-}
-
-const Line = () => (
-  <div className={css(styles.lineContainer)}>
-    <div className={css(styles.line)}/>
-  </div>
-);
-
-const stepSize = 15;
-const styles = StyleSheet.create({
-  upgrade: {
-    marginBottom: 3,
-    paddingBottom: 3,
-    borderBottom: "2px solid gray"
-  },
-
-  upgradeAvatar: {
-    height: "100%"
-  },
-
-  upgradeSequence: {
-    height: stepSize * 1.5,
-    alignItems: "center"
-  },
-
-  lineContainer: {
-    justifyContent: "center"
-  },
-
-  line: {
-    width: stepSize / 2,
-    height: 2,
-    backgroundColor: "gold"
-  },
-
-  step: {
-    width: stepSize,
-    height: stepSize,
-    border: "1px solid gray",
-
-    ":hover": {
-      boxShadow: "0px 0px 4px white"
-    }
-  },
-
-  stepOwned: {
-    backgroundColor: "gold",
-    border: "1px solid gold"
-  },
-
-  stepAvailable: {
-    backgroundColor: "gray"
-  },
-
-  stepLocked: {
-    backgroundColor: "red"
-  }
-});
