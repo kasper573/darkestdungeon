942086f28779194af63569fedd366e6496ece203
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-24 14:29:01 +0100

    Make hibernation test pass

--- src/state/types/EstateEvent.ts
+++ src/state/types/EstateEvent.ts
@@ -2,6 +2,6 @@ import {serializable} from "serializr";
 import {observable} from "mobx";
 
 export class EstateEvent {
-  @serializable @observable message: string;
-  @serializable @observable shown: boolean;
+  @serializable @observable message: string = "";
+  @serializable @observable shown: boolean = false;
 }
