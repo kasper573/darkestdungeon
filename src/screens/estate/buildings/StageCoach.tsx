2b2e082d0bf9c9eccd5d4cd7285f37a2f617a18b
Kasper Sandin <kasper.sandin@gmail.com>
2017-11-29 16:35:02 +0100

    Add horizontal dividers to hero lists in stage coach and graveyard

--- src/screens/estate/buildings/StageCoach.tsx
+++ src/screens/estate/buildings/StageCoach.tsx
@@ -7,6 +7,7 @@ import {StaticState} from "../../../state/StaticState";
 import {css, StyleSheet} from "aphrodite";
 import {BannerHeader} from "../../../ui/BannerHeader";
 import {grid} from "../../../config/Grid";
+import {HorizontalDivider} from "../../../ui/HorizontalDivider";
 
 @observer
 export class StageCoach extends AppStateComponent {
@@ -33,22 +34,34 @@ export class StageCoach extends AppStateComponent {
   }
 
   render () {
+    const lastIndex = this.activeProfile.coach.length - 1;
+    const elements: React.ReactNode[] = [];
+
+    this.activeProfile.coach.forEach((hero, index) => {
+      elements.push(
+        <EstateRosterEntry
+          key={hero.id}
+          hero={hero}
+          transparent={true}
+          onDragEnd={(draggedHero, monitor) => monitor.didDrop() && this.activeProfile.recruitHero(draggedHero)}
+          allowDrag={() => !this.activeProfile.isRosterFull}
+          allowDrop={() => false}
+          classStyle={styles.coachEntry}
+        />
+      );
+
+      if (index !== lastIndex) {
+        elements.push(
+          <HorizontalDivider key={"divider" + index}/>
+        );
+      }
+    });
+
     return (
       <BuildingOverview coverupRight={false} info={StaticState.instance.buildings.get(StageCoach.id)}>
         <div className={css(styles.coachList)}>
           {this.renderMessage()}
-
-          {this.activeProfile.coach.map((hero) => (
-            <EstateRosterEntry
-              key={hero.id}
-              hero={hero}
-              transparent={true}
-              onDragEnd={(draggedHero, monitor) => monitor.didDrop() && this.activeProfile.recruitHero(draggedHero)}
-              allowDrag={() => !this.activeProfile.isRosterFull}
-              allowDrop={() => false}
-              classStyle={styles.coachEntry}
-            />
-          ))}
+          {elements}
         </div>
       </BuildingOverview>
     );
