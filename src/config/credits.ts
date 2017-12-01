ba8c3716f5ea45e0de6adb4764df053d0d505947
Kasper Sandin <kasper.sandin@gmail.com>
2017-12-01 22:17:45 +0100

    Add credits

---
+++ src/config/credits.ts
@@ -0,0 +1,80 @@
+const me = "Kasper Sandin";
+
+export const credits = `
+#Dankest Dungeon
+The intent is to provide consumers with a sense of pride and
+accomplishment for playing a dank ass game in their web browser
+
+#The Dankest Team
+
+Product Owners
+${me}
+
+Producers
+${me}
+
+Development Directors
+${me}
+
+Lead Frontend Engineers
+${me}
+
+Frontend Engineers
+${me}
+
+Office Management
+${me}
+
+Community Management
+${me}
+
+Prototyping
+${me}
+
+UI Components
+${me}
+
+Systems
+${me}
+
+Tools
+${me}
+PuzzledDev
+
+Additional Programming
+${me}
+
+#Special Thanks
+
+Twitch Gorillas
+BTM_pl
+DJShinter
+gene9
+Gr4nnySith
+Kronrat
+SurfSpaniel
+Totttz
+
+Twitch Monkeys
+anthropomorphic_armagedon
+bajerovsky
+coffeedetective
+dav_c
+emerycik
+feelgood12
+graceleslife
+HuntingBlade
+kfirba2
+LaynePavoggi
+Lithzie
+liuting1014
+natsu130
+onixenic
+suplordo
+
+Successful Surgery
+My local hospital
+
+Original Concept, Design & Assets
+Red Hook Studios (please don't sue me <3)
+`;
