1ab5e132d5ed5b4bcdf89adb92438bf2f1ed3716
Kasper Sandin <kasper.sandin@gmail.com>
2017-12-06 06:20:19 +0100

    Correct imports that fuse-box needed fixed

--- assets/sprites.ts
+++ assets/sprites.ts
@@ -3,5 +3,5 @@ import {SpriteInfo} from "./SpriteInfo";
 export const smoke = new SpriteInfo(
   require("./images/smoke.png"), 30, 45,
   {width: 256, height: 256},
-  require("image-size-loader!" + "./images/smoke.png")
+  {width: 1792, height: 1792} //require("image-size-loader!" + "./images/smoke.png")
 );
